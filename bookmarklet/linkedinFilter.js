a4e1a9f68b39d679efedae7063db53fe0205030d
Jason Day <movemaine@gmail.com>
2018-03-05 16:57:16 -0500

    formatting

--- bookmarklet/linkedinFilter.js
+++ bookmarklet/linkedinFilter.js
@@ -1,33 +1,33 @@
 // Bookmarklet Container from Smashing Magazine
 // http://coding.smashingmagazine.com/2010/05/23/make-your-own-bookmarklets-with-jquery/
 
-(function() {
+(function () {
 
-    // the minimum version of jQuery we want
-    var v = "1.6.4";
+	// the minimum version of jQuery we want
+	var v = '1.6.4';
 
-    // check prior inclusion and version
-    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-        var script = document.createElement("script");
-        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-        script.onload = script.onreadystatechange = function() {
-            if ((!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+	// check prior inclusion and version
+	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+		var script = document.createElement('script');
+		script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/' + v + '/jquery.min.js';
+		script.onload = script.onreadystatechange = function () {
+			if ((!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
 
-                initLinkedIn();
-            }
-        };
-        document.getElementsByTagName("head")[0].appendChild(script);
-    } else {
-        initLinkedIn();
-    }
+				initLinkedIn();
+			}
+		};
+		document.getElementsByTagName('head')[0].appendChild(script);
+	} else {
+		initLinkedIn();
+	}
 
-    function initLinkedIn() {
-        (window.linkedinfilter = function() {
+	function initLinkedIn() {
+		(window.linkedinfilter = function () {
 
-            //$('head').append('<link rel="stylesheet" href="" type="text/css" />');
+			//$('head').append('<link rel="stylesheet" href="" type="text/css" />');
 
 
-        })();
-    }
+		})();
+	}
 
 })();
